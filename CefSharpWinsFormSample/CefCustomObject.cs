fb3f440d526d025a41a075bb4447db1e9c9b2e3b
JunHoPark93 <pjh374968@gmail.com>
2018-08-20 06:27:06 -0400

    로컬 리소스 추가, 사용자 정의 Forms 추가 후 크로미움 임베드

---
+++ CefSharpWinsFormSample/CefCustomObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Diagnostics;
+
+namespace CefSharpWinsFormSample
+{
+    class CefCustomObject
+    {
+        // 메인 스레드에서 사용하기 위해 크로미움 로컬 변수 선언
+        private static ChromiumWebBrowser _instanceBrowser = null;
+        // 사용자정의 폼
+        private static Form1 _instanceMainForm = null;
+
+        public CefCustomObject(ChromiumWebBrowser originalBrowser, Form1 mainForm)
+        {
+            _instanceBrowser = originalBrowser;
+            _instanceMainForm = mainForm;
+        }
+
+        /**
+         * 개발자 도구를 보여준다.
+         **/
+        public void showDevTools()
+        {
+            _instanceBrowser.ShowDevTools();
+        }
+
+        /**
+         * 콘솔창 오픈
+         **/
+        public void opencmd()
+        {
+            ProcessStartInfo start = new ProcessStartInfo("cmd.exe", "/c pause");
+            Process.Start(start);
+        }
+
+    }
+}
--- CefSharpWinsFormSample/Form1.cs
+++ CefSharpWinsFormSample/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 using CefSharp;
 using CefSharp.WinForms;
 
@@ -18,26 +20,49 @@ namespace CefSharpWinsFormSample
 
         public Form1()
         {
+            // 레거시 바인딩 에러 https://github.com/cefsharp/CefSharp/issues/2246 이슈 참고
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+
             InitializeComponent();
 
-            InitializeChromium();   
+            InitializeChromium();
+
+            chromeBrowser.RegisterJsObject("cefCustomObject", new CefCustomObject(chromeBrowser, this));
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // 참조에러
+            //chromeBrowser.ShowDevTools();
         }
 
         public void InitializeChromium()
         {
             CefSettings settings = new CefSettings();
-            // Initialize cef with the provided settings
+            Console.WriteLine(Application.StartupPath);
+
+            // 경로 참조 에러
+            //String page = string.Format(@"{0}\html-resources\html\index.html", Application.StartupPath);
+            String page = string.Format(@"C:\Users\Jun Ho Park\source\index.html");
+ 
+            if (!File.Exists(page))
+            {
+                MessageBox.Show("Error The html file doensn't exists : " + page);
+            }
+
             Cef.Initialize(settings);
-            // Create a browser component
-            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
-            // Add it to the form and fill it to the form window.
+
+            chromeBrowser = new ChromiumWebBrowser(page);
+
             this.Controls.Add(chromeBrowser);
             chromeBrowser.Dock = DockStyle.Fill;
+
+            // 로컬 리소스들을 브라우저에서 사용할 수 있게 함
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chromeBrowser.BrowserSettings = browserSettings;
+
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
